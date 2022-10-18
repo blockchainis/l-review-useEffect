40b8c4441065093fca492a9a4782b5c9dd5211d2
hoon2 <sanghoon.engineer@gmail.com>
2022-10-19 07:39:33 +0900

    초기화 상태

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { useState } from "react";
+
 function App() {
-  return <></>;
+  const [query, setQuery] = useState("");
+
+  return (
+    <div>
+      <h1>헤커뉴스 데이터 입니다.</h1>
+      <input
+        onChange={(e) => {
+          setQuery(e.target.value);
+        }}
+        value={query}
+      ></input>
+      <div></div>
+    </div>
+  );
 }
 
 export default App;
